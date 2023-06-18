06fcd5f5ff85c3ded3fd890e543c6dc31c9757f6
hicham <hicha.taha@gmail.com>
2023-06-18 15:30:43 -0700

    comment it out

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -66,7 +66,7 @@ module.exports = () => {
 						options: {
 							presets: ["@babel/preset-env"],
 							plugins: [
-								"@babel/plugin-proposal-object-rest-spread",
+								//"@babel/plugin-proposal-object-rest-spread",
 								"@babel/transform-runtime",
 							],
 						},
